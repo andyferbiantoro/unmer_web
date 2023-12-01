b086f07cc3cffdc1671d599052ffc79bb2548b11
DESKTOP-OAEFTSO\Administrator <andyfebri742@gmail.com>
2023-12-01 16:36:32 +0700

    refresh view

--- resources/views/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="footer-left">
-    <!-- Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Design By <a href="#">STISLA</a> | -->
-</div>
-<div class="footer-right">
-    <!-- 1.0.0 -->
-</div>
