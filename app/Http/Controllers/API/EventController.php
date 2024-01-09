67b24475b8239e458283e66fc506163a59d359ec
anam <anam45188@gmail.com>
2024-01-09 14:43:43 +0700

    event api

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Models\Customer;
 use App\Models\DetailEvent;
 use App\Models\Event;
 use App\Models\TiketEvent;
 use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class EventController extends Controller
 {
@@ -21,7 +24,7 @@ class EventController extends Controller
             $namaBulan = $tanggalObj->format("F");
             
             $v->nama_bulan = $namaBulan;
-            $v->foto = asset('uploads/foto_event/' . $v->foto_event);
+            $v->foto = asset('uploads/event/' . $v->foto_event);
         }
         if ($k) {
 
@@ -41,7 +44,7 @@ class EventController extends Controller
         $k = TiketEvent::where('id_event',$id_event)->orderBy('id','desc')->get();
         foreach($k as $v){
             
-            $v->foto = asset('uploads/foto_event/' . $v->foto_event);
+            $v->foto = asset('uploads/event/' . $v->foto_event);
         }
         if ($k) {
 
@@ -74,4 +77,67 @@ class EventController extends Controller
             ]);
         }
     }
+
+    
+    // public function create_transaki_tiket(Request $request)
+    // {
+    //     $produkIds = [8, 9, 10];
+
+    //     //  return $request->id_product;
+
+
+    //     // return $harga_produk;
+    //     $saldoawal = Customer::where('id_user', $request->id_user)->first();
+
+    //     $transaksi = Transaksi::create([
+    //         'id_user' => $request->id_user,
+    //         'kode_transaksi' =>  strtoupper(Str::random(8)),
+    //         'biaya_layanan'=>$request->biaya_layanan
+    //     ]);
+
+    //     $id_prduc = json_decode($request->id_product);
+    //     $kuantitas = json_decode($request->kuantitas);
+       
+    //     $harga_produk = TiketEvent::whereIn('id', $id_prduc)->get();
+      
+    //     $total = 0;
+    //     foreach ($harga_produk as $n => $p) {
+    //         $qty = $kuantitas[$n];
+    //         $tt = $qty * $p->harga_produk;
+    //         // return $tt;
+    //         $total = $total + $tt;
+
+    //         $req_detail = [
+    //             'id_transaksi_tiket' => $transaksi->id,
+    //             'id_tiket_event' => $p->id,
+    //             'id_event' => $request->id_event,
+    //             'kuantitas' => $qty,
+    //             'total' => $tt
+    //         ];
+    //         Detail::create($req_detail);
+    //     }
+    //     // Keranjang::where('id_user', $request->id_user)->whereIn('id_produk_agrikulture', $id_prduc)->delete();
+    //     Transaksi::where('id', $transaksi->id)->update([
+    //         'nominal' => $total,
+    //         'id_market_agrikulture'=>$harga_produk[0]->id_market
+    //     ]);
+
+    //     $saldokahir = ($saldoawal->saldo - intval($total+$transaksi->biaya_layanan+$transaksi->biaya_ongkir));
+    //     $saldoawal->update([
+    //         'saldo' => $saldokahir
+    //     ]);
+
+    //     $adm =   Admin::where('role_admin','superadmin')->first();
+    //     $akhir = $adm->saldo +$transaksi->biaya_layanan;
+    //     // return $akhir;
+    //     $adm->update([
+    //         'saldo'=>$akhir
+    //     ]);
+
+    //     return response()->json([
+    //         'code' => '200',
+    //         'data' => $transaksi->kode_transaksi
+    //     ]);
+
+    // }
 }
